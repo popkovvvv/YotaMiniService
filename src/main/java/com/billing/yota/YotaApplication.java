c6711eb025c1fb9c40c5f7763d6bcdec0d265c54
popkovvvv <popkovvvv@mail.ru>
2020-02-28 11:38:44 +0300

    Initial commit

---
+++ src/main/java/com/billing/yota/YotaApplication.java
@@ -0,0 +1,13 @@
+package com.billing.yota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/billing/yota/YotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billing.yota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
